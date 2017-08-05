bd5b0f1630708cea4a526dced2296458c0f942e5
Yoseph Wijaya <info@yoseph.ws>
2017-08-05 21:34:51 +0900

    update example project

---
+++ Example/Pods/Target Support Files/Pods-ExampleOfColorPicker/Pods-ExampleOfColorPicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExampleOfColorPicker : NSObject
+@end
+@implementation PodsDummy_Pods_ExampleOfColorPicker
+@end
---
+++ Example/Pods/Target Support Files/YWColorPicker/YWColorPicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YWColorPicker : NSObject
+@end
+@implementation PodsDummy_YWColorPicker
+@end
