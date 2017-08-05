727f574e7f57185807f40b080792d8ac915e73fc
Yoseph Wijaya <info@yoseph.ws>
2017-08-05 19:45:42 +0900

    add initial project

---
+++ YWColorPicker/YWColorPicker.h
@@ -0,0 +1,19 @@
+//
+//  YWColorPicker.h
+//  YWColorPicker
+//
+//  Created by Yoseph Wijaya on 2017/08/05.
+//  Copyright © 2017 Yoseph Wijaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YWColorPicker.
+FOUNDATION_EXPORT double YWColorPickerVersionNumber;
+
+//! Project version string for YWColorPicker.
+FOUNDATION_EXPORT const unsigned char YWColorPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YWColorPicker/PublicHeader.h>
+
+
---
+++ YWColorPickerTests/YWColorPickerTests.swift
@@ -0,0 +1,36 @@
+//
+//  YWColorPickerTests.swift
+//  YWColorPickerTests
+//
+//  Created by Yoseph Wijaya on 2017/08/05.
+//  Copyright © 2017 Yoseph Wijaya. All rights reserved.
+//
+
+import XCTest
+@testable import YWColorPicker
+
+class YWColorPickerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
